54e40421cfad28b4fca0c9186890b46c1f589ef3
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-06 10:51:05 +0530

    Creating Sidebar/Dropdown Menu

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarWrapper,
+  SideBtnWrap,
+} from './SidebarElements';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="Services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
