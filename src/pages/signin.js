3b10755ae75eae679046d681b88ab8677b32ab5e
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-08 15:37:29 +0530

    Creating Sign In Component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignIn from '../components/SignIn/indev';
 
 const SignInPage = () => {
   return (
     <div>
-      <h1>SignIn</h1>
+      <SignIn />
     </div>
   );
 };
